ebdb3f041245d13a3c8f76ec79cc3505d6e7c1a1
ag3nt3154 <ag3nt3154@gmail.com>
2019-09-12 22:22:31 +0800

    testing

--- testing.py
+++ testing.py
@@ -1,1 +1,18 @@
-print('hello world')
+import cv2
+
+# extract the OpenCV version info
+print(cv2.__version__)
+
+OPENCV_OBJECT_TRACKERS = {
+        "csrt": cv2.TrackerCSRT_create,
+        "kcf": cv2.TrackerKCF_create,
+        "boosting": cv2.TrackerBoosting_create,
+        "mil": cv2.TrackerMIL_create,
+        "tld": cv2.TrackerTLD_create,
+        "medianflow": cv2.TrackerMedianFlow_create,
+        "mosse": cv2.TrackerMOSSE_create
+    }
+
+# grab the appropriate object tracker using our dictionary of
+# OpenCV object tracker objects
+tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
