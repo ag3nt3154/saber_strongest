44f4a0bcf0c2e75682c3656f2a672a5d4292ca59
ag3nt3154 <ag3nt3154@gmail.com>
2019-09-17 16:40:52 +0800

    moving boxes

--- misc_fn.py
+++ misc_fn.py
@@ -5,9 +5,9 @@ Created on Mon Apr  3 01:25:04 2017
 @author: alexr
 """
 import json
-
-
-
+import pygame
+import numpy as np
+import time
 
 
 def dump(data, filename):
@@ -26,3 +26,34 @@ def load(filename):
         data = json.load(f)
         return data
 
+
+def generate_box(box_data, displaysurf):
+    # Colour settings
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    gray = (100, 100, 100)
+    yellow = (255, 255, 0)
+    orange = (255, 128, 0)
+    purple = (255, 0, 255)
+    cyan = (0, 255, 255)
+    box_size = 20
+    x = ((box_data[1]+3) % 4) * box_size + 600 - 2 * box_size
+    y = np.floor(box_data[1] / 5) + 350 - box_size
+    if box_data[2] == 0:
+        colour = gray
+    elif box_data[2] == 1:
+        colour = blue
+    elif box_data[2] == 2:
+        colour = purple
+        
+    pygame.draw.rect(displaysurf, colour, (x, y, box_size, box_size), 1)
+    return x, y, colour, box_size
+
+
+def move_box(box, displaysurf):
+    (x, y, colour, box_size) = box
+    
+    pass
--- music/music_test.py
+++ music/music_test.py
@@ -18,16 +18,18 @@ new_divide = {
 
     "artist": "Linkin Park",
     "music_path": "music/new_divide.mp3",
-    "file_path": "new_divide.json",
-    "game_play": {
-        500: [[1, 1, 1]],
-        600: [[2, 2, 2.5]],
-        650: [[1, 1, 1], [2, 2, 2.5]],
-        800: [[1, 1, 1], [2, 1, 1], [3, 1, 1]],
-    }
+    "game_play": [
+        [1000, 1, 1, 1],
+        [2000, 2, 2, 2.5],
+        [3500, 1, 1, 1],
+        [3500, 2, 2, 2.5],
+        [4000, 1, 1, 1],
+        [4000, 2, 1, 1],
+        [4000, 3, 1, 1],
+    ]
 
 }
 
 print(new_divide)
 
-misc_fn.dump(new_divide, new_divide["file_path"])
+misc_fn.dump(new_divide, 'new_divide.json')
